8bb5bae742c48ea8aae27c7de524a612070cf49f
martiiian <jkramorevx@gmail.com>
2020-01-16 05:31:56 +0300

    Create user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 10)->create();
+    }
+}
