99a7c64d38a08e92e52968e1f855d1fd11080a68
martiiian <jkramorevx@gmail.com>
2020-01-18 23:25:40 +0300

    Add resources

---
+++ app/Http/Resources/GoodResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GoodResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'is_visible' => $this->is_visible,
+            'image_name' => $this->image_name,
+            'sizes' => [],
+            'ingredients' => []
+        ];
+    }
+}
