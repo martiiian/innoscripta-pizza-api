51f801c7c33bb63c81df682e4c9e6f0e5019d211
martiiian <jkramorevx@gmail.com>
2020-01-18 23:23:45 +0300

    Add migration

---
+++ database/migrations/2020_01_18_184250_create_goods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->boolean('is_visible');
+            $table->string('image_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
