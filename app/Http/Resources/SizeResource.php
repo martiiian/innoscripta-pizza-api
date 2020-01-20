33f517bad6e10e9fbd29a894f7b9949476bb76c7
martiiian <jkramorevx@gmail.com>
2020-01-20 13:53:59 +0300

    Add id to resources

--- app/Http/Resources/SizeResource.php
+++ app/Http/Resources/SizeResource.php
@@ -15,6 +15,7 @@ class SizeResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'code' => $this->code
         ];
