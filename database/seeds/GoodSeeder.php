0e83ccd2b6dd54216858ce9d968074d3928fd911
martiiian <jkramorevx@gmail.com>
2020-01-22 14:04:13 +0300

    Add name and image to goods seeder

--- database/seeds/GoodSeeder.php
+++ database/seeds/GoodSeeder.php
@@ -5,6 +5,31 @@ use \App\Good;
 
 class GoodSeeder extends Seeder
 {
+    public $path = '/goods/';
+    public $images = [
+        'alfredo.jpg',
+        'carbonara.jpg',
+        'fermer.jpg',
+        'gavaiskay.jpg',
+        'home.jpg',
+        'hunter.jpg',
+        'peperoni.jpg',
+        'vegetarian.jpg'
+    ];
+
+    public $names = [
+        'Alfredo',
+        'Carbonara',
+        'Fermer',
+        'Gavaisaya',
+        'Homemade',
+        'Hunter',
+        'Peperoni',
+        'Vegetarian'
+    ];
+
+    public $index = 0;
+
     /**
      * Run the database seeds.
      *
@@ -12,6 +37,13 @@ class GoodSeeder extends Seeder
      */
     public function run()
     {
-        factory(Good::class, 8)->create();
+        factory(Good::class, 8)
+            ->create()
+            ->each(function ($product) {
+                $product->name = $this->names[$this->index];
+                $product->image_name = $this->path . $this->images[$this->index];
+                $product->save();
+                $this->index++;
+            });
     }
 }
