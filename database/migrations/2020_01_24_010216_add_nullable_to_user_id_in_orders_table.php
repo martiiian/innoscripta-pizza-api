53518e95eca510b935d714d7168eda189d848515
martiiian <jkramorevx@gmail.com>
2020-01-24 04:24:37 +0300

    Fix user_id nullable migration

--- database/migrations/2020_01_24_010216_add_nullable_to_user_id_in_orders_table.php
+++ database/migrations/2020_01_24_010216_add_nullable_to_user_id_in_orders_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Order;
+use App\OrderGoods;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -27,6 +29,14 @@ class AddNullableToUserIdInOrdersTable extends Migration
      */
     public function down()
     {
+        $nullable_orders_model = Order::whereNull('user_id');
+        $nullable_orders_id_arr = array_map(function ($order) {
+            return $order['id'];
+        }, $nullable_orders_model->get()->toArray());
+
+        OrderGoods::whereIn('order_id', $nullable_orders_id_arr)->delete();
+        $nullable_orders_model->delete();
+
         Schema::table('orders', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id')
                 ->nullable(false)
