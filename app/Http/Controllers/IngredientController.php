447de1d70188f863c7563ca183555829791204b9
martiiian <jkramorevx@gmail.com>
2020-01-24 04:33:43 +0300

    Remove unused methods

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Ingredient;
-use Illuminate\Http\Request;
 use App\Http\Resources\Ingredient as IngredientResource;
 
 class IngredientController extends Controller
@@ -17,29 +16,10 @@ class IngredientController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param int $id
-     * @return \App\Http\Resources\Ingredient|array
+     * @return IngredientResource|array
      */
     public function show($id)
     {
@@ -48,36 +28,5 @@ class IngredientController extends Controller
         return $ingredient->count() > 0
             ? new IngredientResource($ingredient)
             : [];
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- app/Http/Controllers/SizeController.php
+++ app/Http/Controllers/SizeController.php
@@ -4,37 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\SizeResource;
 use App\Size;
-use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class SizeController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
         return SizeResource::collection(Size::all());
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     /**
@@ -49,39 +30,5 @@ class SizeController extends Controller
         return $size->count() > 0
             ? new SizeResource($size)
             : [];
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
