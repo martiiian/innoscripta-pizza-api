7e7fe8179c80ca44ad32ecf6ec6ca658ea95ddc9
martiiian <jkramorevx@gmail.com>
2020-01-24 07:20:19 +0300

    Add registration method

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
@@ -40,18 +41,15 @@ class User extends Authenticatable implements JWTSubject
 
     public static function store($data)
     {
-        $user = User::where('phone', $data['phone'])->first();
-        if (!$user) {
-            $user = new self;
-            $user->fill([
-                'name' => $data['name'],
-                'email' => $data['email'] ?? null,
-                'phone' => $data['phone'],
-                'address' => $data['address'] ?? '',
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
-            ]);
-            $user->save();
-        }
+        $user = new self;
+        $user->fill([
+            'name' => $data['name'],
+            'email' => $data['email'] ?? null,
+            'phone' => $data['phone'],
+            'address' => $data['address'] ?? '',
+            'password' => Hash::make($data['password'])
+        ]);
+        $user->save();
         return $user;
     }
 
